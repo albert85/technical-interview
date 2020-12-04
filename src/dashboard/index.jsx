c748f19ad884507225e5d2329d4a22ce32a43c6e
albert85 <okeola85>
2020-12-04 12:45:32 +0100

    implement third section

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,16 +1,413 @@
-import { Box, Grid } from '@material-ui/core'
-import React from 'react'
-import { DashboardContainer } from './style'
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import { Box, Grid, InputAdornment } from '@material-ui/core';
+import React from 'react';
+import CloseIcon from '@material-ui/icons/Close';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import PixIcon from '../assets/pix1.png';
+import Pix2Icon from '../assets/px2.png';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TuneIcon from '@material-ui/icons/Tune';
+import BgIcon from '../assets/bg.png';
+import ManIcon from '../assets/man.png';
+import {
+  DashboardContainer,
+  EachDot,
+  ExpandArrow,
+  ExpandArrowContainer,
+  Eyes,
+  FirstCard,
+  InnerActiveContainer,
+  OuterActiveContainer,
+  SeenContainer,
+  SeenText,
+  SliderContainer,
+  YogaCard,
+  YogaText,
+  YogaSubText,
+  DetailsCard,
+  FollowerText,
+  CloseContainer,
+  ImageContainer,
+  DividerContainer,
+  AnalyticsText,
+  AnalyticsSubText,
+  FollowBtn,
+  CourseBorder,
+  ChipText,
+  PlayerContainer,
+  PlayerContent,
+  PlayerText,
+  PlayerSubText,
+  PlayerBorder,
+  PlayerIcon,
+  BigPlayContainer,
+  BigPlayerIcon,
+  OverlayContainer,
+  HorizontalTabs,
+  DiscussionContainer,
+  DiscussionText,
+  ActiveIcon,
+} from './style';
+import InputField from '../components/InputField';
+import { useForm } from 'react-hook-form';
+import { ArrowForward } from '@material-ui/icons';
 
 export default function Dashboard() {
+  const { control, errors } = useForm({
+    defaultValues: {
+      search: '',
+    },
+  });
+  const RenderPlayerContainer = ({ src, title, author }) => {
+    return (
+      <PlayerContent>
+        <Box
+          py="5px"
+          px="10px"
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box>
+            <img src={src} alt="image" />
+          </Box>
+          <Box>
+            <PlayerText>{title}</PlayerText>
+            <PlayerSubText>{author}</PlayerSubText>
+          </Box>
+          <Box>
+            <PlayerBorder>
+              <PlayerIcon />
+            </PlayerBorder>
+          </Box>
+        </Box>
+      </PlayerContent>
+    );
+  };
   return (
     <DashboardContainer>
-     <Grid container>
-       <Grid item></Grid>
-       <Grid item></Grid>
-       <Grid item></Grid>
-     </Grid>
-      
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12} md={4} style={{ marginBottom: '10%' }}>
+          <FirstCard>
+            <SliderContainer>
+              <Box px="10px" display="flex">
+                <EachDot />
+                <EachDot />
+                <EachDot
+                  inputcolor="#A0A9B9"
+                  style={{ width: '140px', borderRadius: '20px' }}
+                >
+                  <EachDot style={{ width: '30px', borderRadius: '20px' }} />
+                </EachDot>
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+              </Box>
+            </SliderContainer>
+            <Box display="flex" justifyContent="flex-end" px="30px" mt="20px">
+              <ExpandArrowContainer>
+                <ExpandArrow />
+              </ExpandArrowContainer>
+            </Box>
+            <SeenContainer>
+              <Eyes />
+              <SeenText>950</SeenText>
+            </SeenContainer>
+            <YogaCard>
+              <Box pt="10px" display="flex" justifyContent="center" mb="10px">
+                <EachDot
+                  inputcolor="#dce3ef"
+                  style={{ width: '30px', borderRadius: '20px' }}
+                />
+              </Box>
+              <Box display="flex" justifyContent="space-between" pr="30px">
+                <Box>
+                  <Box display="flex" pl="20px" mt="10px" alignItems="center">
+                    <Box>
+                      <YogaText>Yoga at Home</YogaText>
+                    </Box>
+                    <Box ml="10px">
+                      <OuterActiveContainer>
+                        <InnerActiveContainer />
+                      </OuterActiveContainer>
+                    </Box>
+                  </Box>
+                  <Box pl="20px">
+                    <YogaSubText>Sarah Cooper</YogaSubText>
+                  </Box>
+                </Box>
+                <Box>
+                  <img width="60px" src={BgIcon} alt="placeholder" />
+                </Box>
+              </Box>
+            </YogaCard>
+          </FirstCard>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Box>
+            <DetailsCard>
+              <Box p="20px">
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="flex-start"
+                >
+                  <ImageContainer>
+                    <img
+                      width="50px"
+                      src={
+                        'https://www.clipartmax.com/png/full/223-2236392_female-headshot-placeholder-female-profile-icon-png.png'
+                      }
+                      alt="placeholder"
+                    />
+                    <ActiveIcon />
+                  </ImageContainer>
+                  <Box>
+                    <YogaText>Sarah Cooper</YogaText>
+                    <YogaSubText>Veteran Yoga Instructor</YogaSubText>
+                    <FollowerText>3.2k Followers</FollowerText>
+                  </Box>
+                  <Box>
+                    <CloseContainer>
+                      <CloseIcon style={{ fontSize: '12px' }} />
+                    </CloseContainer>
+                  </Box>
+                </Box>
+                <DividerContainer />
+                <Box display="flex" justifyContent="space-between">
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    flexDirection="column"
+                  >
+                    <AnalyticsText>3700+</AnalyticsText>
+                    <AnalyticsSubText>Students</AnalyticsSubText>
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    flexDirection="column"
+                  >
+                    <AnalyticsText>13</AnalyticsText>
+                    <AnalyticsSubText>Courses</AnalyticsSubText>
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    flexDirection="column"
+                  >
+                    <AnalyticsText>22hrs</AnalyticsText>
+                    <AnalyticsSubText>Videos</AnalyticsSubText>
+                  </Box>
+                </Box>
+                <DividerContainer />
+                <Box
+                  width="100%"
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Box display="flex">
+                    <TwitterIcon />
+                    <Box ml="20px">
+                      <InstagramIcon />
+                    </Box>
+                  </Box>
+                  <Box>
+                    <FollowBtn
+                      startIcon={<AddIcon style={{ color: '#fff' }} />}
+                    >
+                      Follow
+                    </FollowBtn>
+                  </Box>
+                </Box>
+              </Box>
+            </DetailsCard>
+          </Box>
+          <Box mt="20px">
+            <DetailsCard>
+              <Box display="flex" justifyContent="space-between">
+                <YogaText>Course Catalogue</YogaText>
+                <TuneIcon
+                  style={{ transform: 'rotate(90deg)', color: '#e17674' }}
+                />
+              </Box>
+              <Box display="flex" mt="10px">
+                <CourseBorder>
+                  <Box display="flex" alignItems="center">
+                    <ChipText>Peace</ChipText>
+                    <CloseIcon style={{ fontSize: '14px' }} />
+                  </Box>
+                </CourseBorder>
+                <CourseBorder>
+                  <Box display="flex" alignItems="center">
+                    <ChipText>Short Duration</ChipText>
+                    <CloseIcon style={{ fontSize: '14px' }} />
+                  </Box>
+                </CourseBorder>
+              </Box>
+              <PlayerContainer>
+                <RenderPlayerContainer
+                  src={PixIcon}
+                  title="Midnight Bliss"
+                  author="David Gilmour"
+                />
+                <RenderPlayerContainer
+                src={Pix2Icon}
+                title="Visualization 101"
+                  author="Harsimran Singh"
+                />
+                <RenderPlayerContainer
+                src={Pix2Icon}
+                title="Meditation Basic"
+                author="David Gilmour"
+                />
+              </PlayerContainer>
+            </DetailsCard>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Box height="92vh">
+          <FirstCard style={{ height: '300px', minHeight: '340px' }}>
+            <SliderContainer>
+              <Box px="10px" display="flex">
+                <EachDot />
+                <EachDot />
+                <EachDot
+                  inputcolor="#A0A9B9"
+                  style={{ width: '140px', borderRadius: '20px' }}
+                >
+                  <EachDot style={{ width: '50px', borderRadius: '20px' }} />
+                </EachDot>
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+                <EachDot inputcolor="#A0A9B9" />
+              </Box>
+            </SliderContainer>
+            <OverlayContainer>
+              <Box display="flex" justifyContent="center" px="30px">
+                <BigPlayContainer>
+                  <BigPlayerIcon fontSize="large" />
+                </BigPlayContainer>
+              </Box>
+              <SeenContainer style={{ marginTop: '28%' }}>
+                <Eyes />
+                <SeenText>974</SeenText>
+              </SeenContainer>
+            </OverlayContainer>
+            <YogaCard
+              style={{ margin: '0', paddingBottom: '10px', bottom: '-100%' }}
+            >
+              <Box pt="10px" display="flex" justifyContent="center" mb="10px">
+                <EachDot
+                  inputcolor="#dce3ef"
+                  style={{ width: '30px', borderRadius: '20px' }}
+                />
+              </Box>
+              <Box display="flex" justifyContent="space-between" pr="30px">
+                <Box>
+                  <Box display="flex" pl="20px" mt="10px" alignItems="center">
+                    <Box>
+                      <YogaText>Yoga at Home</YogaText>
+                    </Box>
+                    <Box ml="10px">
+                      <OuterActiveContainer>
+                        <InnerActiveContainer />
+                      </OuterActiveContainer>
+                    </Box>
+                  </Box>
+                  <Box pl="20px">
+                    <YogaSubText>Sarah Cooper</YogaSubText>
+                  </Box>
+                </Box>
+                <Box>
+                  <img width="60px" src={BgIcon} alt="placeholder" />
+                </Box>
+              </Box>
+              <DiscussionContainer>
+                <HorizontalTabs>
+                  <CourseBorder>
+                    <Box display="flex" alignItems="center">
+                      <ChipText>Discussion</ChipText>
+                      <ExpandMoreIcon style={{ fontSize: '14px' }} />
+                    </Box>
+                  </CourseBorder>
+                  <Box display="flex" alignItems="center" pr="15px">
+                    <ChipText>Details</ChipText>
+                  </Box>
+                  <Box display="flex" alignItems="center" pr="15px">
+                    <ChipText>Transcript</ChipText>
+                  </Box>
+                  <Box display="flex" alignItems="center" pr="15px">
+                    <ChipText>NoteBooks</ChipText>
+                  </Box>
+                </HorizontalTabs>
+                <Box>
+                  <Box display="flex" alignItems="center">
+                    <Box pl="20px">
+                      <img width="60px" src={ManIcon} alt="placeholder" />
+                    </Box>
+                    <Box px="10px">
+                      <PlayerSubText>Samuel K</PlayerSubText>
+                      <DiscussionText>
+                        Thank you for this live course as now we can workout
+                        during this pandemic
+                      </DiscussionText>
+                    </Box>
+                  </Box>
+                  <Box display="flex" alignItems="center">
+                    <Box pl="20px">
+                      <img width="60px" src={BgIcon} alt="placeholder" />
+                    </Box>
+                    <Box px="10px">
+                      <PlayerSubText>Misha Singh</PlayerSubText>
+                      <DiscussionText>
+                        Hello from India. Thank you for this session
+                      </DiscussionText>
+                    </Box>
+                  </Box>
+                  <Box px="20px">
+                    <InputField
+                      errors={errors}
+                      control={control}
+                      inputName="search"
+                      textProps={{
+                        placeholder: 'Type Comment',
+                        type: 'email',
+                        InputProps: {
+                          style: {
+                            backgroundColor: '#ECF0F3',
+                            borderRadius: '10px',
+                            fontSize: '12px',
+                            padding: '5px 0',
+                            marginBottom: '20px',
+                          },
+                          endAdornment: (
+                            <InputAdornment>
+                              <ArrowForward style={{ color: '#e17674' }} />
+                            </InputAdornment>
+                          ),
+                        },
+                      }}
+                    />
+                  </Box>
+                </Box>
+              </DiscussionContainer>
+            </YogaCard>
+          </FirstCard>
+          </Box>
+        </Grid>
+      </Grid>
     </DashboardContainer>
-  )
+  );
 }
--- src/dashboard/style.js
+++ src/dashboard/style.js
@@ -1,6 +1,292 @@
 import styled from 'styled-components';
+import HeightIcon from '@material-ui/icons/Height';
+import VisibilityOutlinedIcon from '@material-ui/icons/VisibilityOutlined';
+import { Button, Typography } from '@material-ui/core';
+import { PlayArrow } from '@material-ui/icons';
 
 export const DashboardContainer = styled.div`
-  background-color: #D5EFCA;
-  height: 100vh;
+  background-color: #d5efca;
+  padding: 40px;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+
+export const FirstCard = styled.div`
+  width: 300px;
+  border-radius: 30px;
+  min-height: 600px;
+  background-image: url('https://im.whatshot.in/img/2020/Mar/erik-brolin-wmznegkcsmw-unsplash-cropped-1584700767.jpg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+`;
+
+export const SliderContainer = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 60px;
+  border-radius: 30px 30px 0 0;
+  display: flex;
+  align-items: center;
+  border: none !important;
+`;
+
+export const EachDot = styled.div`
+  border-radius: 50%;
+  height: 5px;
+  width: 5px;
+  margin-right: 10px;
+  background-color: ${({ inputcolor }) => inputcolor || '#fff'};
+`;
+
+export const ExpandArrow = styled(HeightIcon)`
+  transform: rotate(45deg);
+  color: #fff;
+`;
+
+export const Eyes = styled(VisibilityOutlinedIcon)`
+  color: #fff;
+  font-size: 12px !important;
+`;
+
+export const ExpandArrowContainer = styled.div`
+  border-radius: 50%;
+  height: 40px;
+  width: 40px;
+  background-color: rgba(255, 255, 255, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SeenContainer = styled.div`
+  margin-top: 420px;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 20px;
+  align-items: center;
+`;
+
+export const SeenText = styled(Typography)`
+  color: #fff;
+  margin-left: 10px !important;
+  font-size: 12px !important;
+`;
+
+export const YogaCard = styled.div`
+  border-radius: 20px;
+  box-shadow: 1px 1px 10px #e4e4e4;
+  width: 100%;
+  min-height: 120px;
+  position: absolute;
+  background-color: #fff;
+  margin-top: 5px;
+`;
+
+export const YogaText = styled(Typography)`
+  font-weight: bold !important;
+  font-size: 19px;
+`;
+
+export const YogaSubText = styled(Typography)`
+  font-weight: bold !important;
+  font-size: 12px !important;
+  color: #b2b5b9;
+`;
+
+export const OuterActiveContainer = styled.div`
+  height: 7px;
+  width: 7px;
+  background-color: #f9cac4;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InnerActiveContainer = styled.div`
+  height: 3px;
+  width: 3px;
+  background-color: #e17674;
+  border-radius: 50%;
+`;
+
+export const DetailsCard = styled.div`
+  background-color: #fff;
+  border-radius: 20px;
+  padding: 20px;
+  width: 260px;
+`;
+
+export const FollowerText = styled(Typography)`
+  font-size: 10px !important;
+  color: green;
+  font-weight: bold !important;
+`;
+
+export const CloseContainer = styled.div`
+  border-radius: 50%;
+  background-color: #efe6e6;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 20px;
+  width: 20px;
+`;
+
+export const ImageContainer = styled.div`
+  border-radius: 50%;
+  width: 60px;
+  background-color: #13bf7b;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+export const DividerContainer = styled.hr`
+  border: 0.1px solid #e4e4e4;
+  margin: 30px 0px 20px 0 !important;
+`;
+
+export const AnalyticsText = styled(Typography)`
+  font-weight: bold !important;
+  font-size: 18px !important;
+`;
+
+export const AnalyticsSubText = styled(Typography)`
+  font-size: 12px !important;
+  font-weight: 500 !important;
+  color: #989898;
+`;
+
+export const FollowBtn = styled(Button)`
+  text-transform: none !important;
+  background-color: #e17674 !important;
+  color: #fff !important;
+  border-radius: 10px !important;
+  font-size: 12px !important;
+  padding: 5px 12px !important;
+`;
+
+export const CourseBorder = styled.div`
+  border-radius: 10px;
+  padding: 4px 12px;
+  border: 1px solid #e17674;
+  display: inline-block;
+  margin-right: 10px !important;
+`;
+
+export const ChipText = styled(Typography)`
+  font-size: 12px !important;
+  font-weight: 500 !important;
+  color: #989898;
+`;
+
+export const PlayerText = styled(Typography)`
+  font-size: 16px !important;
+  font-weight: bold !important;
+`;
+
+export const PlayerSubText = styled(Typography)`
+  font-size: 12px !important;
+  font-weight: bold !important;
+  color: #afafaf;
+`;
+
+export const PlayerContainer = styled.div`
+  height: 200px;
+  margin-top: 30px;
+  overflow: scroll;
+  background-color: #f6f6f8;
+  padding: 0 10px;
+`;
+
+export const PlayerContent = styled.div`
+  border-radius: 10px;
+  width: 100%;
+  min-height: 50px;
+  background-color: #fff;
+  margin-bottom: 15px !important;
+  margin-top: 15px !important;
+`;
+
+export const PlayerImg = styled.div`
+  border-radius: 10px;
+  width: 50px;
+  height: 50px;
+  background-image: url(${({ src }) => src});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+`;
+
+export const PlayerBorder = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 2px solid #e17674;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+export const PlayerIcon = styled(PlayArrow)`
+  color: #e17674;
+  cursor: pointer;
+`;
+
+export const BigPlayerIcon = styled(PlayArrow)`
+  color: #fff;
+  cursor: pointer;
+`;
+
+export const BigPlayContainer = styled.div`
+  border-radius: 50%;
+  height: 80px;
+  width: 80px;
+  background-color: rgba(255, 255, 255, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 36%;
+`;
+
+export const OverlayContainer = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  height: 100%;
+`;
+
+export const HorizontalTabs = styled.div`
+  padding: 5px 5px 5px 20px;
+  margin: 5px;
+  display: flex;
+  overflow: scroll;
+`;
+
+export const DiscussionContainer = styled.div`
+  background-color: #fff;
+  border-radius: 20px;
+  box-shadow: 1px -5px 3px #e4e4e4;
+`;
+
+export const DiscussionText = styled(Typography)`
+  font-size: 11px !important;
+  font-weight: bold !important;
+`;
+
+export const ActiveIcon = styled.div`
+  height: 6px;
+  width: 6px;
+  border: 2px solid #fff;
+  background-color: #e17674;
+  position: absolute;
+  border-radius: 50%;
+  top: 6%;
+  left: 3%;
 `;
